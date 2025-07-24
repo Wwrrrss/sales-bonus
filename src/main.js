4385a5bacf62bca4cff291239f0b9c68dd9d3cba
Tanya <tylucsijc@yandex.ru>
2025-07-24 18:05:45 +0300

    done

--- src/main.js
+++ src/main.js
@@ -92,7 +92,7 @@ function analyzeSalesData(data, options) {
         seller.top_products = Object.entries(seller.products_sold)
         .map(([sku, quantity]) => ({ sku, quantity }))
         .sort((a, b) => b.quantity - a.quantity)
-        .slice(10)
+        .slice(0, 10)
     });
 
     return sellerStats.map(seller => ({
